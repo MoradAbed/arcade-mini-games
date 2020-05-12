fc918615bc6d9a9dbacc4360923178d1768f2eae
moris <moris.rafol@gmail.com>
2020-05-12 15:00:55 +0300

    feat: LoginForm.js, LoginForm.css  implementation

--- src/components/interface/LoginForm.js
+++ src/components/interface/LoginForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import "../../index.css";
+import "./LoginForm.css";
 
 
 function LoginForm(props) {
     return (
-        <div></div>
+        <div className="formContainer">
+            <form className="loginForm">
+                <label htmlFor="username">Enter your github username</label>
+                <input name="username" type="text"/>
+                <input type="submit" name="submit" value="log in"/>
+            </form>
+        </div>
+
     );
 }
 
