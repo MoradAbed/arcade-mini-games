c0d917f9fea7dffa425cc3a7af9131b8063a464f
moris <moris.rafol@gmail.com>
2020-05-13 18:24:37 +0300

    feat: working RPS

---
+++ src/components/RPS/RPS.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import RpsRound from "./rpsSelect/RpsRound";
+import GameIntro from "../commonGameElements/gameIntro/GameIntro";
+import "./rps.css"
+import ResultPopup from "../interface/resultPopup/ResultPopup";
+
+const states = {
+    intro : 0,
+    inGame : 1,
+    endGame: 2
+}
+
+function RPS() {
+
+    const [gameState, setGameState] = useState(states.intro);
+    const [gameResult, setGameResult] = useState();
+
+    useEffect(()=>{
+
+        if(gameResult)
+            setTimeout(()=>{setGameState(states.endGame)},1000)
+
+    }, [ gameResult])
+
+
+    if(gameState === states.intro)
+        return (
+            <div className="gameContainer">
+                <GameIntro title="you're gonna lose" onStart={()=>{ setGameState(states.inGame)}}/>
+            </div>
+        );
+
+
+    if(gameState === states.inGame)
+        return (
+            <div  className="gameContainer">
+                <RpsRound  roundTime={5} onRoundEnd={(res)=> setGameResult(res)}/>
+            </div>
+        );
+
+
+    if(gameState === states.endGame){
+        return   <div  className="gameContainer">
+                <ResultPopup
+                    btnText={"restart"}
+                    onResolve={()=>{setGameState(states.intro)}}
+                             content={gameResult.draw? "it's a draw!!": gameResult.playerWon?"you won!!!" : "you lost :("} />
+            </div>
+    }
+
+}
+
+export default RPS;
